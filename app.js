6c38ac93fecb29fa2cfde83186662e8f3a115966
Forhad Hossain <forhadhossain0619@gmail.com>
2023-10-21 10:04:10 -0700

    add heading class

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const wrapper = React.createElement('div',{className:'wrapper'},'',
     [React.createElement('div',{className:'parent'},'',
-    React.createElement('h1',{},'I am parent'),
+    React.createElement('h1',{className:'parent-title'},'I am parent'),
     React.createElement('p',{},'Barir Korta')
     )],
     [
         React.createElement('div',{className:'parent-two'},'',
-        React.createElement('h1',{},'I am parent two'),
+        React.createElement('h1',{className:'parent-title'},'I am parent two'),
         React.createElement('p',{},'Barir Second Korta'))
     ]
     
